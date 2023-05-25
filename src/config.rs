e728d361149611e411cf8233db5e89b8d60371c0
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-05-25 12:19:23 +0300

    added project config file loading

--- src/config.rs
+++ src/config.rs
@@ -22,4 +22,19 @@ impl Config {
         let config = toml::from_str(&file_contents)?;
         Ok(config)
     }
+
+    pub fn get_config_files() -> anyhow::Result<Vec<String>> {
+        let config_files = ["Cargo.toml", "package.json"];
+
+        let current_dir = std::env::current_dir()?;
+        let files = std::fs::read_dir(current_dir)?
+            .filter_map(|entry| {
+                let file_name = entry.expect("Failed to read entry").file_name();
+                file_name.to_string_lossy().to_string().into()
+            })
+            .filter(|file_name| config_files.contains(&file_name.as_str()))
+            .collect::<Vec<String>>();
+
+        Ok(files)
+    }
 }
